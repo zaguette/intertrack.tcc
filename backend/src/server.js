f85d17cc1f668f5cfcd134e0850d3a386ad94854
giucei <giucei>
2026-02-11 09:38:55 -0300

    inicio de tcc, criação das pastas de backend e frontend

---
+++ backend/src/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/',(req, res)=>{
+    res.json({"mensagem": "Hello World"})
+})
+
+app.listen(PORT, () =>{
+    console.log("IT'S ALIVE!!!!!")
+})
