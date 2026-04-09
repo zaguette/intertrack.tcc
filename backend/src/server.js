81084910898bf001b7affc21a9e12b71f29003b7
giu_cei <giulia.cei1@eaportal.org>
2026-04-09 11:58:25 -0300

    instalação do CORS

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,22 +4,27 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes.js';
 import encomendaRoutes from './routes/encomendaRoutes.js';
 
+// Carrega as variáveis de ambiente do arquivo .env
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+// Boa prática: usa a porta do .env se existir, senão usa a 3000
+const PORT = process.env.PORT || 3000;
 
+// Middlewares
 app.use(express.json());
 app.use(cors());
 
-// Registrando as rotas (Apenas uma vez cada!)
+// Registrando as rotas
 app.use('/usuarios', userRoutes);
 app.use('/encomendas', encomendaRoutes);
 
+// Rota principal (Health Check)
 app.get('/', (req, res) => {
     res.json({ mensagem: "Intertrack API online! 🚀" });
 });
 
+// Iniciando o servidor
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
