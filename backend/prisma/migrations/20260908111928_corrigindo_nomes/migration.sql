86857b59b842cf1a6fb1824185aba094ae2b9085
hfcosta <hfcosta>
2026-09-08 08:23:25 -0300

    arrumando migrations prisma db

--- backend/prisma/migrations/20260908111928_corrigindo_nomes/migration.sql
+++ backend/prisma/migrations/20260908111928_corrigindo_nomes/migration.sql
@@ -1,21 +1,24 @@
 -- CreateTable
-CREATE TABLE `Usuario` (
+CREATE TABLE `usuario` (
     `id` CHAR(36) NOT NULL,
-    `RA` VARCHAR(191) NOT NULL,
+    `codigo` VARCHAR(191) NOT NULL,
+    `ra` VARCHAR(30) NOT NULL,
     `nome` VARCHAR(100) NOT NULL,
     `email` VARCHAR(100) NOT NULL,
     `senha` VARCHAR(255) NOT NULL,
+    `telefone` VARCHAR(30) NULL,
     `ativo` BOOLEAN NOT NULL DEFAULT true,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL,
 
-    UNIQUE INDEX `Usuario_RA_key`(`RA`),
-    UNIQUE INDEX `Usuario_email_key`(`email`),
+    UNIQUE INDEX `usuario_codigo_key`(`codigo`),
+    UNIQUE INDEX `usuario_ra_key`(`ra`),
+    UNIQUE INDEX `usuario_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `Funcionario` (
+CREATE TABLE `funcionario` (
     `id` CHAR(36) NOT NULL,
     `codigo` VARCHAR(191) NOT NULL,
     `nome` VARCHAR(100) NOT NULL,
@@ -27,24 +30,24 @@ CREATE TABLE `Funcionario` (
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL,
 
-    UNIQUE INDEX `Funcionario_codigo_key`(`codigo`),
-    UNIQUE INDEX `Funcionario_email_key`(`email`),
+    UNIQUE INDEX `funcionario_codigo_key`(`codigo`),
+    UNIQUE INDEX `funcionario_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `StatusEncomenda` (
+CREATE TABLE `status_encomenda` (
     `id` CHAR(36) NOT NULL,
     `codigo` VARCHAR(191) NOT NULL,
     `nome_status` VARCHAR(50) NOT NULL,
     `descricao` VARCHAR(255) NULL,
 
-    UNIQUE INDEX `StatusEncomenda_codigo_key`(`codigo`),
+    UNIQUE INDEX `status_encomenda_codigo_key`(`codigo`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `Remetente` (
+CREATE TABLE `remetente` (
     `id` CHAR(36) NOT NULL,
     `codigo` VARCHAR(191) NOT NULL,
     `nome` VARCHAR(100) NOT NULL,
@@ -52,12 +55,12 @@ CREATE TABLE `Remetente` (
     `site` VARCHAR(255) NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
-    UNIQUE INDEX `Remetente_codigo_key`(`codigo`),
+    UNIQUE INDEX `remetente_codigo_key`(`codigo`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `NomeAlternativo` (
+CREATE TABLE `nome_alternativo` (
     `id` CHAR(36) NOT NULL,
     `codigo` VARCHAR(191) NOT NULL,
     `usuario_id` CHAR(36) NOT NULL,
@@ -68,13 +71,13 @@ CREATE TABLE `NomeAlternativo` (
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL,
 
-    UNIQUE INDEX `NomeAlternativo_codigo_key`(`codigo`),
+    UNIQUE INDEX `nome_alternativo_codigo_key`(`codigo`),
     INDEX `NomeAlternativo_usuario_id_fkey`(`usuario_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `Encomenda` (
+CREATE TABLE `encomenda` (
     `id` CHAR(36) NOT NULL,
     `codigo` VARCHAR(191) NOT NULL,
     `codigo_rastreio` VARCHAR(50) NULL,
@@ -88,9 +91,9 @@ CREATE TABLE `Encomenda` (
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL,
 
-    UNIQUE INDEX `Encomenda_codigo_key`(`codigo`),
-    INDEX `Encomenda_codigo_rastreio_idx`(`codigo_rastreio`),
-    INDEX `Encomenda_destinatario_usuario_id_idx`(`destinatario_usuario_id`),
+    UNIQUE INDEX `encomenda_codigo_key`(`codigo`),
+    INDEX `encomenda_codigo_rastreio_idx`(`codigo_rastreio`),
+    INDEX `encomenda_destinatario_usuario_id_idx`(`destinatario_usuario_id`),
     INDEX `Encomenda_funcionario_id_fkey`(`funcionario_id`),
     INDEX `Encomenda_remetente_id_fkey`(`remetente_id`),
     INDEX `Encomenda_status_atual_id_fkey`(`status_atual_id`),
@@ -98,7 +101,7 @@ CREATE TABLE `Encomenda` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `HistoricoStatus` (
+CREATE TABLE `historico_status` (
     `id` CHAR(36) NOT NULL,
     `codigo` VARCHAR(191) NOT NULL,
     `encomenda_id` CHAR(36) NOT NULL,
@@ -106,15 +109,15 @@ CREATE TABLE `HistoricoStatus` (
     `alterado_por` CHAR(36) NOT NULL,
     `data_alteracao` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
-    UNIQUE INDEX `HistoricoStatus_codigo_key`(`codigo`),
-    INDEX `HistoricoStatus_encomenda_id_idx`(`encomenda_id`),
+    UNIQUE INDEX `historico_status_codigo_key`(`codigo`),
+    INDEX `historico_status_encomenda_id_idx`(`encomenda_id`),
     INDEX `HistoricoStatus_alterado_por_fkey`(`alterado_por`),
     INDEX `HistoricoStatus_status_id_fkey`(`status_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `Notificacao` (
+CREATE TABLE `notificacao` (
     `id` CHAR(36) NOT NULL,
     `codigo` VARCHAR(191) NOT NULL,
     `usuario_id` CHAR(36) NOT NULL,
@@ -124,14 +127,14 @@ CREATE TABLE `Notificacao` (
     `lida` BOOLEAN NOT NULL DEFAULT false,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
-    UNIQUE INDEX `Notificacao_codigo_key`(`codigo`),
-    INDEX `Notificacao_usuario_id_idx`(`usuario_id`),
+    UNIQUE INDEX `notificacao_codigo_key`(`codigo`),
+    INDEX `notificacao_usuario_id_idx`(`usuario_id`),
     INDEX `Notificacao_encomenda_id_fkey`(`encomenda_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `Autorizacao` (
+CREATE TABLE `autorizacao` (
     `id` CHAR(36) NOT NULL,
     `codigo` VARCHAR(191) NOT NULL,
     `encomenda_id` CHAR(36) NOT NULL,
@@ -143,15 +146,15 @@ CREATE TABLE `Autorizacao` (
     `utilizada` BOOLEAN NOT NULL DEFAULT false,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
-    UNIQUE INDEX `Autorizacao_codigo_key`(`codigo`),
-    INDEX `Autorizacao_encomenda_id_idx`(`encomenda_id`),
+    UNIQUE INDEX `autorizacao_codigo_key`(`codigo`),
+    INDEX `autorizacao_encomenda_id_idx`(`encomenda_id`),
     INDEX `Autorizacao_autorizado_por_fkey`(`autorizado_por`),
     INDEX `Autorizacao_destinatario_id_fkey`(`destinatario_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `Auditoria` (
+CREATE TABLE `auditoria` (
     `id` CHAR(36) NOT NULL,
     `codigo` VARCHAR(191) NOT NULL,
     `funcionario_id` CHAR(36) NOT NULL,
@@ -162,49 +165,49 @@ CREATE TABLE `Auditoria` (
     `ip_usuario` VARCHAR(45) NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
-    UNIQUE INDEX `Auditoria_codigo_key`(`codigo`),
+    UNIQUE INDEX `auditoria_codigo_key`(`codigo`),
     INDEX `Auditoria_funcionario_id_fkey`(`funcionario_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `NomeAlternativo` ADD CONSTRAINT `NomeAlternativo_usuario_id_fkey` FOREIGN KEY (`usuario_id`) REFERENCES `Usuario`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `nome_alternativo` ADD CONSTRAINT `nome_alternativo_usuario_id_fkey` FOREIGN KEY (`usuario_id`) REFERENCES `usuario`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Encomenda` ADD CONSTRAINT `Encomenda_destinatario_usuario_id_fkey` FOREIGN KEY (`destinatario_usuario_id`) REFERENCES `Usuario`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `encomenda` ADD CONSTRAINT `encomenda_destinatario_usuario_id_fkey` FOREIGN KEY (`destinatario_usuario_id`) REFERENCES `usuario`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Encomenda` ADD CONSTRAINT `Encomenda_funcionario_id_fkey` FOREIGN KEY (`funcionario_id`) REFERENCES `Funcionario`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `encomenda` ADD CONSTRAINT `encomenda_funcionario_id_fkey` FOREIGN KEY (`funcionario_id`) REFERENCES `funcionario`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Encomenda` ADD CONSTRAINT `Encomenda_remetente_id_fkey` FOREIGN KEY (`remetente_id`) REFERENCES `Remetente`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `encomenda` ADD CONSTRAINT `encomenda_remetente_id_fkey` FOREIGN KEY (`remetente_id`) REFERENCES `remetente`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Encomenda` ADD CONSTRAINT `Encomenda_status_atual_id_fkey` FOREIGN KEY (`status_atual_id`) REFERENCES `StatusEncomenda`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `encomenda` ADD CONSTRAINT `encomenda_status_atual_id_fkey` FOREIGN KEY (`status_atual_id`) REFERENCES `status_encomenda`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `HistoricoStatus` ADD CONSTRAINT `HistoricoStatus_alterado_por_fkey` FOREIGN KEY (`alterado_por`) REFERENCES `Funcionario`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `historico_status` ADD CONSTRAINT `historico_status_alterado_por_fkey` FOREIGN KEY (`alterado_por`) REFERENCES `funcionario`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `HistoricoStatus` ADD CONSTRAINT `HistoricoStatus_encomenda_id_fkey` FOREIGN KEY (`encomenda_id`) REFERENCES `Encomenda`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `historico_status` ADD CONSTRAINT `historico_status_encomenda_id_fkey` FOREIGN KEY (`encomenda_id`) REFERENCES `encomenda`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `HistoricoStatus` ADD CONSTRAINT `HistoricoStatus_status_id_fkey` FOREIGN KEY (`status_id`) REFERENCES `StatusEncomenda`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `historico_status` ADD CONSTRAINT `historico_status_status_id_fkey` FOREIGN KEY (`status_id`) REFERENCES `status_encomenda`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Notificacao` ADD CONSTRAINT `Notificacao_encomenda_id_fkey` FOREIGN KEY (`encomenda_id`) REFERENCES `Encomenda`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `notificacao` ADD CONSTRAINT `notificacao_encomenda_id_fkey` FOREIGN KEY (`encomenda_id`) REFERENCES `encomenda`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Notificacao` ADD CONSTRAINT `Notificacao_usuario_id_fkey` FOREIGN KEY (`usuario_id`) REFERENCES `Usuario`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `notificacao` ADD CONSTRAINT `notificacao_usuario_id_fkey` FOREIGN KEY (`usuario_id`) REFERENCES `usuario`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Autorizacao` ADD CONSTRAINT `Autorizacao_autorizado_por_fkey` FOREIGN KEY (`autorizado_por`) REFERENCES `Usuario`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `autorizacao` ADD CONSTRAINT `autorizacao_autorizado_por_fkey` FOREIGN KEY (`autorizado_por`) REFERENCES `usuario`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Autorizacao` ADD CONSTRAINT `Autorizacao_destinatario_id_fkey` FOREIGN KEY (`destinatario_id`) REFERENCES `NomeAlternativo`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE `autorizacao` ADD CONSTRAINT `autorizacao_destinatario_id_fkey` FOREIGN KEY (`destinatario_id`) REFERENCES `nome_alternativo`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Autorizacao` ADD CONSTRAINT `Autorizacao_encomenda_id_fkey` FOREIGN KEY (`encomenda_id`) REFERENCES `Encomenda`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `autorizacao` ADD CONSTRAINT `autorizacao_encomenda_id_fkey` FOREIGN KEY (`encomenda_id`) REFERENCES `encomenda`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Auditoria` ADD CONSTRAINT `Auditoria_funcionario_id_fkey` FOREIGN KEY (`funcionario_id`) REFERENCES `Funcionario`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `auditoria` ADD CONSTRAINT `auditoria_funcionario_id_fkey` FOREIGN KEY (`funcionario_id`) REFERENCES `funcionario`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
